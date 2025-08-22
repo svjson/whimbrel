2b23d8e3f70c6d941084eb31c3ee13d8edba62eb
Sven Johansson <sven.johansson@iteam.se>
2025-08-22 20:17:45 +0200

    Let gitignore:create report contributors

--- packages/facets/gitignore/src/tasks/create.ts
+++ packages/facets/gitignore/src/tasks/create.ts
@@ -2,20 +2,36 @@ import path from 'node:path'
 
 import { makeTask, WhimbrelContext } from '@whimbrel/core-api'
 import { queryFacets } from '@whimbrel/facet'
+import { beginFlow } from '@whimbrel/flow'
 
 export const GITIGNORE__CREATE = 'gitignore:create'
 
 const execute = async (ctx: WhimbrelContext) => {
   const { actor } = ctx.step.inputs
 
-  const queryResult = await queryFacets(ctx, actor, {
-    type: 'version-control:ignore-files',
-  })
-
-  const merged = queryResult.flatMap((qr) => qr.result)
-
-  const contents = ['# --- Ignore Files ---', ...merged.map((p) => p.pattern), '']
-  await ctx.disk.write(path.join(actor.root, '.gitignore'), contents.join('\n'), 'utf8')
+  await beginFlow(ctx)
+    .let(
+      'queryResult',
+      queryFacets(ctx, actor, {
+        type: 'version-control:ignore-files',
+      }),
+      true
+    )
+    .let('contributors', ({ queryResult }) => queryResult.map((qr) => qr.source))
+    .let('ignoreFiles', ({ queryResult }) => queryResult.flatMap((qr) => qr.result), true)
+    .do(async ({ ignoreFiles }) => {
+      const contents = [
+        '# --- Ignore Files ---',
+        ...ignoreFiles.map((p) => p.pattern),
+        '',
+      ]
+      await ctx.disk.write(
+        path.join(actor.root, '.gitignore'),
+        contents.join('\n'),
+        'utf8'
+      )
+    })
+    .run()
 }
 
 export const Create = makeTask({
