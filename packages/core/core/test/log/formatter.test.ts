f63f3cc7d4e47d31387f2471d72176a738aa945e
Sven Johansson <sven.johansson@iteam.se>
2025-08-22 20:18:51 +0200

    DefaultFormatter tests

---
+++ packages/core/core/test/log/formatter.test.ts
@@ -0,0 +1,122 @@
+import { newStepResult } from '@whimbrel/core-api'
+import { describe, expect, it } from 'vitest'
+import { makeWhimbrelContext, DefaultFormatter } from '@src/index'
+
+describe('DefaultFormatter', () => {
+  describe('formatStepResult', () => {
+    it('should return an empty string when given an empty stepResult', async () => {
+      // Given
+      const ctx = await makeWhimbrelContext({})
+      const formatter = new DefaultFormatter(ctx)
+      const stepResult = newStepResult()
+
+      // When
+      const formatted = formatter.formatStepResult(stepResult)
+
+      // Then
+      expect(formatted).toEqual('')
+    })
+  })
+
+  describe('formatContextMutations', () => {
+    it('should format a `set` mutation on a single line', async () => {
+      // Given
+      const ctx = await makeWhimbrelContext({})
+      const formatter = new DefaultFormatter(ctx)
+      const stepResult = newStepResult()
+      stepResult.mutations.ctx.push({
+        mutationType: 'ctx',
+        type: 'set',
+        path: 'source',
+        key: 'banana-boat',
+      })
+
+      // When
+      const formatted = formatter.formatStepResult(stepResult)
+
+      // Then
+      expect(formatted).toEqual('⇒ source: banana-boat')
+    })
+
+    it('should format multiple `set` mutations on separate lines', async () => {
+      // Given
+      const ctx = await makeWhimbrelContext({})
+      const formatter = new DefaultFormatter(ctx)
+      const stepResult = newStepResult()
+      stepResult.mutations.ctx.push(
+        {
+          mutationType: 'ctx',
+          type: 'set',
+          path: 'source',
+          key: 'secret-informer',
+        },
+        {
+          mutationType: 'ctx',
+          type: 'set',
+          path: 'target',
+          key: 'undercover-agent',
+        }
+      )
+
+      // When
+      const formatted = formatter.formatStepResult(stepResult)
+
+      // Then
+      expect(formatted).toEqual('⇒ source: secret-informer\n⇒ target: undercover-agent')
+    })
+
+    it('should format an `add` and a `set` of the same key as a single `add`', async () => {
+      // Given
+      const ctx = await makeWhimbrelContext({})
+      const formatter = new DefaultFormatter(ctx)
+      const stepResult = newStepResult()
+      stepResult.mutations.ctx.push(
+        {
+          mutationType: 'ctx',
+          type: 'add',
+          path: 'source',
+          key: 'secret-informer',
+        },
+        {
+          mutationType: 'ctx',
+          type: 'set',
+          path: 'source',
+          key: 'secret-informer',
+        }
+      )
+
+      // When
+      const formatted = formatter.formatStepResult(stepResult)
+
+      // Then
+      expect(formatted).toEqual('⊕ source: secret-informer')
+    })
+
+    it('should format an `add` and a `set` of different keys on separate lines', async () => {
+      // Given
+      const ctx = await makeWhimbrelContext({})
+      const formatter = new DefaultFormatter(ctx)
+      const stepResult = newStepResult()
+      stepResult.mutations.ctx.push(
+        {
+          mutationType: 'ctx',
+          type: 'add',
+          path: 'source',
+          key: 'secret-informer',
+        },
+        {
+          mutationType: 'ctx',
+          type: 'set',
+          path: 'target',
+          key: 'blubber',
+        }
+      )
+
+      // When
+      const formatted = formatter.formatStepResult(stepResult)
+
+      // Then
+      expect(formatted).toEqual('⊕ source: secret-informer\n⇒ target: blubber')
+    })
+  })
+})
