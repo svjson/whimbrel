630092e292309516d2d55914fbde723201d1743a
Sven Johansson <sven.johansson@iteam.se>
2025-08-22 17:06:49 +0200

    Let gitignore:create use facet queries to determine file contents

--- packages/facets/gitignore/src/tasks/create.ts
+++ packages/facets/gitignore/src/tasks/create.ts
@@ -1,12 +1,20 @@
 import path from 'node:path'
 
 import { makeTask, WhimbrelContext } from '@whimbrel/core-api'
+import { queryFacets } from '@whimbrel/facet'
 
 export const GITIGNORE__CREATE = 'gitignore:create'
 
 const execute = async (ctx: WhimbrelContext) => {
   const { actor } = ctx.step.inputs
-  const contents = ['# --- Misc ---', '*.log', '']
+
+  const queryResult = await queryFacets(ctx, actor, {
+    type: 'version-control:ignore-files',
+  })
+
+  const merged = queryResult.flatMap((qr) => qr.result)
+
+  const contents = ['# --- Ignore Files ---', ...merged.map((p) => p.pattern), '']
   await ctx.disk.write(path.join(actor.root, '.gitignore'), contents.join('\n'), 'utf8')
 }
 
