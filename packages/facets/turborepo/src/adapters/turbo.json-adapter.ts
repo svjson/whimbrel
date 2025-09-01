2fc82e0f4374f9723d5ae88dce206a4c8c62d42e
Sven Johansson <sven.johansson@iteam.se>
2025-09-01 20:57:27 +0200

    turborepo: Using generator functinos to provide turbo.json readIfExists and read

--- packages/facets/turborepo/src/adapters/turbo.json-adapter.ts
+++ packages/facets/turborepo/src/adapters/turbo.json-adapter.ts
@@ -1,37 +1,11 @@
-import path from 'node:path'
-
-import { FileSystem, WhimbrelError } from '@whimbrel/core-api'
-import { JSONFile } from '@whimbrel/struct-file'
-import { ifFileExistsAt } from '@whimbrel/filesystem'
+import { JSONFile, makeRead, makeReadIfExists } from '@whimbrel/struct-file'
 
 export class TurboJSON extends JSONFile {
-  constructor({
-    path = '',
-    content,
-    disk = null,
-  }: {
-    path?: string
-    content: string | any
-    disk?: FileSystem
-  }) {
-    super({ path, content, storage: disk })
-  }
+  static readIfExists = makeReadIfExists(TurboJSON, 'turbo.json', async (disk, fPath) =>
+    disk.read(fPath, 'utf8')
+  )
 
-  static async readIfExists(disk: FileSystem, filePath: string | string[]) {
-    return await ifFileExistsAt(disk, filePath, 'turbo.json', async (fPath) => {
-      return new TurboJSON({
-        path: fPath,
-        disk,
-        content: await disk.readJson(fPath),
-      })
-    })
-  }
-
-  static async read(disk: FileSystem, filePath: string | string[]): Promise<TurboJSON> {
-    const pkgJson = await this.readIfExists(disk, filePath)
-    if (pkgJson) return pkgJson
-    throw new WhimbrelError(
-      `turbo.json file not found: ${Array.isArray(filePath) ? path.join(...filePath) : filePath}`
-    )
-  }
+  static read = makeRead(TurboJSON, 'turbo.json', async (disk, fPath) =>
+    disk.read(fPath, 'utf8')
+  )
 }
