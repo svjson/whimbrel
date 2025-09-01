e76d12c89100c2300a79fab6abd6fa94740420f1
Sven Johansson <sven.johansson@iteam.se>
2025-09-01 21:03:00 +0200

    filesystem: Remove redundant ifFileExistsAt

--- packages/core/filesystem/src/index.ts
+++ packages/core/filesystem/src/index.ts
@@ -1,23 +1,4 @@
-import path from 'node:path'
-import { FileSystem } from '@whimbrel/core-api'
-
 export { ContextFileSystem } from './context'
 export { DiskFileSystem, PhysicalFileSystem } from './physical'
 export { MemoryFileSystem } from './memfs'
 export { FileSystemReporter, FileSystemMutationReporter } from './reporter'
-
-export const ifFileExistsAt = async <T>(
-  disk: FileSystem,
-  filePath: string | string[],
-  fileName: string,
-  fn: (fPath: string) => Promise<T>
-) => {
-  let actualPath = Array.isArray(filePath) ? path.join(...filePath) : filePath
-  if (path.basename(actualPath) !== fileName) {
-    actualPath = path.join(actualPath, fileName)
-  }
-
-  if (await disk.exists(actualPath)) {
-    return fn(actualPath)
-  }
-}
